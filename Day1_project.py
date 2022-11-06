02b8b6e99bce0a1e9139588b8efc72fda4b37dc5
mregmi22 <112472631+mregmi22@users.noreply.github.com>
2022-11-06 16:06:59 -0700

    Add files via upload

---
+++ Day1_project.py
@@ -0,0 +1,11 @@
+# helping people to come up with a band name when they can't think of one by asking questions about themselves
+
+#prompt user for name
+print("Welcome to band name genenator ")
+city = input ("which city did you grew in? \n") 
+pet = input ("what is your pet name? \n")
+#prints band name by string concatenating pet and city 
+print("Your band name could be " +   city +  " " + pet)
+
+
+
